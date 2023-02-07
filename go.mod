ab9a89375694b3d6489e21c9ffe57f1383316022
Cleison Ferreira de Melo <cleisommais@gmail.com>
2023-02-07 03:30:40 +0000

    Included postgres connection

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/cleisommais/oauth-service-v1
 go 1.19
 
 require github.com/gorilla/mux v1.8.0
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.7
+)
--- main.go
+++ main.go
@@ -3,17 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"log"
 	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"github.com/cleisommais/oauth-service-v1/routes"
+	"github.com/cleisommais/oauth-service-v1/db"	
 )
 
 func main() {
+	errEnvLoad := godotenv.Load()
+	if errEnvLoad != nil {
+		log.Fatalf("Error loading .env file: %v", errEnvLoad)
+	}	
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8000"
 	}
+	conn, err := db.CreatePostgresConnection()
+	if err != nil {
+		log.Fatalf("Error creating database connection: %v", err)
+	}
+	defer conn.Close()	
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -22,8 +34,8 @@ func main() {
 	r.HandleFunc("/test", routes.TestHandler).Methods("GET")
 
 	fmt.Println("Listening on port", port)
-	err := http.ListenAndServe(":"+port, r)
-	if err != nil {
-		fmt.Println(err)
+	httpErr := http.ListenAndServe(":"+port, r)
+	if httpErr != nil {
+		fmt.Println(httpErr)
 	}
 }
